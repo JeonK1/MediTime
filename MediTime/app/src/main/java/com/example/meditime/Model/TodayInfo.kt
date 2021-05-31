c5af602c6652b7baa288db6531bdd43b8ac30fa9
kimhyeonmyeong <khm4188@naver.com>
2021-06-01 04:11:48 +0900

    [update] three major view's title color design update & Today's alarm view update
    [update message]
    - three major view's title color design update
     세개 주요화면의 타이틀 부분 색깔이 전계원씨 말씀처럼 너무 밝은 것 같아서 일단 좀 더 진한 컬러로 디자인 해보았습니다.
    
    - Today's alarm view update
     '오늘'화면에서 alarm이 뿌려지는 형식을 update하였습니다. 기존 구현방식을 하나의 약알람 모양에서 '1일 2회'복용의 경우 약알람 내부에서 텍스트 증가형식으로
     구현하려고 했었습니다. 다만, 이런 방식은 UI를 난잡하게 만들 수 있습니다. 고로 처음 만들어두었던 JOIN문을 활용해서 '1일 2회'복용의 경우처럼 여러번 복용해야할 경우,
     '오늘'에서 여러번 약알람이 등장하도록 하였습니다. 앞으로는 '오늘' 약알람을 누를 경우 발생할 event에 대해서 손볼것입니다.

--- MediTime/app/src/main/java/com/example/meditime/Model/TodayInfo.kt
+++ MediTime/app/src/main/java/com/example/meditime/Model/TodayInfo.kt
@@ -4,6 +4,12 @@ import java.io.Serializable
 
 data class TodayInfo(
     val medi_no: Int,
+    val time_no: Int,
     val medi_name: String,
-    var time_list:ArrayList<TodayTimeInfo> = ArrayList<TodayTimeInfo>()
+    val take_date: String,
+    val set_date: String,
+    val set_check: Int
+
 ): Serializable
+
+
